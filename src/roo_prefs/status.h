4c22a1972ff63bc98d51b77cc69e75202a54ecee
dejwk <dawid.kurzyniec@gmail.com>
2026-02-26 02:40:03 +0100

    Added compatibility shims.

--- src/roo_prefs/status.h
+++ src/roo_prefs/status.h
@@ -11,4 +11,23 @@ enum class WriteResult { kOk, kError };
 /// Result of clearing a value from storage.
 enum class ClearResult { kOk, kError };
 
+/// @deprecated Use `ReadResult::kOk` instead.
+constexpr ReadResult READ_OK = ReadResult::kOk;
+/// @deprecated Use `ReadResult::kNotFound` instead.
+constexpr ReadResult READ_NOT_FOUND = ReadResult::kNotFound;
+/// @deprecated Use `ReadResult::kWrongType` instead.
+constexpr ReadResult READ_WRONG_TYPE = ReadResult::kWrongType;
+/// @deprecated Use `ReadResult::kError` instead.
+constexpr ReadResult READ_ERROR = ReadResult::kError;
+
+/// @deprecated Use `WriteResult::kOk` instead.
+constexpr WriteResult WRITE_OK = WriteResult::kOk;
+/// @deprecated Use `WriteResult::kError` instead.
+constexpr WriteResult WRITE_ERROR = WriteResult::kError;
+
+/// @deprecated Use `ClearResult::kOk` instead.
+constexpr ClearResult CLEAR_OK = ClearResult::kOk;
+/// @deprecated Use `ClearResult::kError` instead.
+constexpr ClearResult CLEAR_ERROR = ClearResult::kError;
+
 }  // namespace roo_prefs
