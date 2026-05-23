61bc3ed51acf93fcd3b4adb34cb5cdc74877244e
Dawid Kurzyniec <dawid.kurzyniec@gmail.com>
2026-05-23 14:41:35 +0200

    More flexible serialization: moved to a separate file and out of 'internal'.

--- src/roo_prefs/pref.h
+++ src/roo_prefs/pref.h
@@ -5,6 +5,7 @@
 #include <string>
 
 #include "roo_prefs/collection.h"
+#include "roo_prefs/serialization.h"
 #include "roo_prefs/status.h"
 #include "roo_prefs/transaction.h"
 
@@ -88,172 +89,6 @@ using String = Pref<std::string>;
 
 /// Implementation details follow.
 
-namespace internal {
-
-inline ClearResult StoreClear(Store& store, const char* key) {
-  return store.clear(key);
-}
-
-template <typename T>
-inline WriteResult StoreWrite(Store& store, const char* key, const T& val) {
-  return store.writeObject(key, val);
-}
-
-template <>
-inline WriteResult StoreWrite<bool>(Store& store, const char* key,
-                                    const bool& val) {
-  return store.writeBool(key, val);
-}
-
-template <>
-inline WriteResult StoreWrite<uint8_t>(Store& store, const char* key,
-                                       const uint8_t& val) {
-  return store.writeU8(key, val);
-}
-
-template <>
-inline WriteResult StoreWrite<int8_t>(Store& store, const char* key,
-                                      const int8_t& val) {
-  return store.writeI8(key, val);
-}
-
-template <>
-inline WriteResult StoreWrite<uint16_t>(Store& store, const char* key,
-                                        const uint16_t& val) {
-  return store.writeU16(key, val);
-}
-
-template <>
-inline WriteResult StoreWrite<int16_t>(Store& store, const char* key,
-                                       const int16_t& val) {
-  return store.writeI16(key, val);
-}
-
-template <>
-inline WriteResult StoreWrite<uint32_t>(Store& store, const char* key,
-                                        const uint32_t& val) {
-  return store.writeU32(key, val);
-}
-
-template <>
-inline WriteResult StoreWrite<int32_t>(Store& store, const char* key,
-                                       const int32_t& val) {
-  return store.writeI32(key, val);
-}
-
-template <>
-inline WriteResult StoreWrite<uint64_t>(Store& store, const char* key,
-                                        const uint64_t& val) {
-  return store.writeU64(key, val);
-}
-
-template <>
-inline WriteResult StoreWrite<int64_t>(Store& store, const char* key,
-                                       const int64_t& val) {
-  return store.writeI64(key, val);
-}
-
-template <>
-inline WriteResult StoreWrite<float>(Store& store, const char* key,
-                                     const float& val) {
-  return store.writeFloat(key, val);
-}
-
-template <>
-inline WriteResult StoreWrite<double>(Store& store, const char* key,
-                                      const double& val) {
-  return store.writeDouble(key, val);
-}
-
-template <>
-inline WriteResult StoreWrite<roo::string_view>(Store& store, const char* key,
-                                                const roo::string_view& val) {
-  return store.writeString(key, val);
-}
-
-template <>
-inline WriteResult StoreWrite<std::string>(Store& store, const char* key,
-                                           const std::string& val) {
-  return store.writeString(key, val);
-}
-
-template <typename T>
-inline ReadResult StoreRead(Store& store, const char* key, T& val) {
-  return store.readObject(key, val);
-}
-
-template <>
-inline ReadResult StoreRead<bool>(Store& store, const char* key, bool& val) {
-  return store.readBool(key, val);
-}
-
-template <>
-inline ReadResult StoreRead<uint8_t>(Store& store, const char* key,
-                                     uint8_t& val) {
-  return store.readU8(key, val);
-}
-
-template <>
-inline ReadResult StoreRead<int8_t>(Store& store, const char* key,
-                                    int8_t& val) {
-  return store.readI8(key, val);
-}
-
-template <>
-inline ReadResult StoreRead<uint16_t>(Store& store, const char* key,
-                                      uint16_t& val) {
-  return store.readU16(key, val);
-}
-
-template <>
-inline ReadResult StoreRead<int16_t>(Store& store, const char* key,
-                                     int16_t& val) {
-  return store.readI16(key, val);
-}
-
-template <>
-inline ReadResult StoreRead<uint32_t>(Store& store, const char* key,
-                                      uint32_t& val) {
-  return store.readU32(key, val);
-}
-
-template <>
-inline ReadResult StoreRead<int32_t>(Store& store, const char* key,
-                                     int32_t& val) {
-  return store.readI32(key, val);
-}
-
-template <>
-inline ReadResult StoreRead<uint64_t>(Store& store, const char* key,
-                                      uint64_t& val) {
-  return store.readU64(key, val);
-}
-
-template <>
-inline ReadResult StoreRead<int64_t>(Store& store, const char* key,
-                                     int64_t& val) {
-  return store.readI64(key, val);
-}
-
-template <>
-inline ReadResult StoreRead<float>(Store& store, const char* key, float& val) {
-  return store.readFloat(key, val);
-}
-
-template <>
-inline ReadResult StoreRead<double>(Store& store, const char* key,
-                                    double& val) {
-  return store.readDouble(key, val);
-}
-
-template <>
-inline ReadResult StoreRead<std::string>(Store& store, const char* key,
-                                         std::string& val) {
-  return store.readString(key, val);
-}
-
-}  // namespace internal
-
 template <typename T>
 Pref<T>::Pref(Collection& collection, const char* key, T default_value)
     : collection_(collection),
@@ -285,7 +120,7 @@ bool Pref<T>::set(const T& value) {
     state_ = PrefState::kError;
     return false;
   }
-  switch (internal::StoreWrite(t.store(), key_, value)) {
+  switch (StoreWrite(t.store(), key_, value)) {
     case WriteResult::kOk: {
       value_ = value;
       state_ = PrefState::kSet;
@@ -309,7 +144,7 @@ bool Pref<T>::clear() {
     state_ = PrefState::kError;
     return false;
   }
-  switch (internal::StoreClear(t.store(), key_)) {
+  switch (StoreClear(t.store(), key_)) {
     case ClearResult::kOk: {
       state_ = PrefState::kUnset;
       value_ = default_value_;
@@ -331,7 +166,7 @@ void Pref<T>::sync() const {
       value_ = default_value_;
       return;
     }
-    switch (internal::StoreRead(t.store(), key_, value_)) {
+    switch (StoreRead(t.store(), key_, value_)) {
       case ReadResult::kOk: {
         state_ = PrefState::kSet;
         return;
